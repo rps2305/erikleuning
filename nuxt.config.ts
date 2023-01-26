367be583dd0372567109ce15df5b36888131c80d
Ronald Punt <ronald.punt@mac.com>
2023-01-26 21:28:54 +0100

    First Commit

---
+++ nuxt.config.ts
@@ -0,0 +1,19 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    css: ['~/assets/css/main.css'],
+    generate:{
+      nojekyll: true, //not working on this version
+      fallback: '404.html',
+    },
+    ssr: false, //When false
+    target: "static", // and static, nuxt generates a SPA
+    postcss: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      },
+      app: {
+        baseURL: "/erikleuning/"
+    }
+})
