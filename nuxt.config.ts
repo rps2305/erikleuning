680471e7e09e3783953900bdcf28b0240df3cb0a
Ronald Punt <ronald.punt@mac.com>
2023-01-26 22:32:21 +0100

    generated7

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     css: ['~/assets/css/main.css'],
-    generate:{
-      nojekyll: true, //not working on this version
-      fallback: '404.html',
+    router: {
+      base: '/erikleuning/'
     },
-    
 
     postcss: {
         plugins: {
@@ -13,7 +11,5 @@ export default defineNuxtConfig({
           autoprefixer: {},
         },
       },
-      app: {
-        baseURL: "/erikleuning/"
-    }
+
 })
