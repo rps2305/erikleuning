442611ee349ec466a93da6edf06c6d847f6a0285
Twence BV <monitor@twence.nl>
2023-01-27 10:42:08 +0100

    css changed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    css: ['~/assets/css/main.css'],
+    css: ['~/assets/css/main.css','/erikleuning/assets/css/main.css'],
     generate:{
       nojekyll: true, //not working on this version
       fallback: '404.html',
